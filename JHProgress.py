679426cfa177d9b1068029ccdb78c22362306a71
JuanC <palajnc2@gmail.com>
2025-11-30 21:01:11 -0500

    scrapping de la sec

---
+++ JHProgress.py
@@ -0,0 +1,102 @@
+import requests
+import pandas as pd
+from io import StringIO
+import json
+import polars as pl
+import yaml
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class JHProgress:
+
+    def __init__(self) -> None:
+        with open("config.yaml", "r") as f:
+            self.config = yaml.safe_load(f)
+
+        self.tickers_cik= self._connect_tickers()
+        self.df_fundamentals=self._df_fundamentals()
+  
+        
+
+
+    def _connect_tickers(self,debug=False) -> pd.DataFrame:
+        print('Downloading tickers from the SEC API')
+        zeros_mapper={5:'00000',6:'0000',7:'000',8:'00'}
+
+        headers={'User-Agent':'palajnc@gmail.com'}
+        tickers=requests.get('https://www.sec.gov/files/company_tickers.json',headers=headers)
+        tickers=pd.json_normalize(pd.json_normalize(tickers.json(),max_level=0).values[0])
+        tickers['cik_lenght']=tickers['cik_str'].astype(str).apply(len)
+        tickers['zeros_lenght']=tickers['cik_lenght'].map(zeros_mapper)
+        tickers['cik_str']=f'CIK'+tickers['zeros_lenght']+tickers['cik_str'].astype(str)
+        
+        if debug:
+            tickers.head(10)
+        logging.info('Tickers downloaded successfully')
+        return tickers
+
+    #Scrappea la información de la SEC
+    def scrapping_sec(self,symbol: str)-> dict:
+        cik=self.tickers_cik[self.tickers_cik['ticker']==symbol]['cik_str'].values[0]
+        url=f'https://data.sec.gov/api/xbrl/companyfacts/{cik}.json'
+
+        headers={'User-Agent':'palajnc@gmail.com'}
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Raise an error for bad responses
+            data = response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data for {symbol}: {e}")
+            data={}
+        logging.info(f'Data for {symbol} fetched successfully from SEC')
+        return data
+    
+    def obtener_caracteristica(self, symbol: str, caracteristica: str) -> pd.DataFrame:
+        data = self.scrapping_sec(symbol)
+        resultado = self._dfs(data, caracteristica)
+        try:
+            df=pd.json_normalize(resultado)
+        except:
+            df=pd.DataFrame()
+
+        for c in df.columns:
+            try:
+                df=df.explode(c)
+                df=pd.json_normalize(df.to_dict(orient='records'))
+            except:
+                pass
+        logging.info(f'Caracteristica {caracteristica} obtenida para {symbol}')
+        return df
+
+    def _dfs(self,node, target):
+        if isinstance(node, dict):
+            for key, value in node.items():
+                if key == target:
+                    return value
+                result = self._dfs(value, target)
+                if result is not None:
+                    return result
+        elif isinstance(node, list):
+            for item in node:
+                result = self._dfs(item, target)
+                if result is not None:
+                    return result
+        return None
+    
+    def _df_fundamentals(self) -> pd.DataFrame:
+        df=pd.DataFrame()
+        for symbol in self.config['Simbolos']:
+            for metrica in self.config['Metricas']:
+                logging.debug(f'Obteniendo {metrica} para {symbol}')
+                df_temp=self.obtener_caracteristica(symbol,metrica)
+                df_temp['Symbol']=symbol
+                df_temp['Metrica']=metrica
+                df=pd.concat([df,df_temp],ignore_index=True)
+        logging.info('DataFrame de fundamentales creado exitosamente')
+        return df
+
+
+        
+
+
